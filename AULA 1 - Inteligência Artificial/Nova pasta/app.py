ba787435930af1e1f57abdd501560467fe91a320
lilibritosilva10-blip <lilibritosilva10@gmail.com>
2026-07-22 18:02:39 -0300

    Add files via upload

---
+++ AULA 1 - Inteligência Artificial/Nova pasta/app.py
@@ -0,0 +1,22 @@
+
+
+
+import tkinter as tk 
+def display()
+     mostrar_texto = dado.get()
+janela = tk.Tk() # criar uma janela 
+
+janela.geometry('500x500')
+
+tk.Label(janela, text= 'FORMULARIO').pack()
+
+janela.mainloop()
+dado = tk.Entry(janela)
+dado.pack(pady=s)
+
+btn = tk.Button(janela, text = 'clique aqui')
+btn.pack()
+mostrar = tk.Label(janela. texte''')
+janela.mainloop()
+btn = tk.Button(janela, text = 'enviar')
+btn.pack
